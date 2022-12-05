2c9daede624b58e4b29f974b37454795b34a2b59
Davi Kennedy Alves Rodrigues <davi_kennedy@live.com>
2022-12-05 12:46:54 -0300

    Commit inicial

---
+++ bytebankAdm/Funcionarios/Funcionario.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace bytebankAdm.Funcionarios
+{
+    public class Funcionario
+    {
+        public string Nome { get; set; }
+        public string Cpf { get; set; }
+        public double Salario { get; set; }
+        private int _tipo;
+
+        public double getBonificacao()
+        {
+            return Salario * 0.1;
+        }
+    }
+}
---
+++ bytebankAdm/Program.cs
@@ -0,0 +1,25 @@
+﻿using bytebankAdm.Funcionarios;
+
+Console.WriteLine("Boas vindas ao Bytebank Administração!\n");
+
+Funcionario f1 = new Funcionario();
+f1.Nome = "João";
+f1.Cpf = "039.745.842-63";
+f1.Salario = 2000;
+
+Console.WriteLine($"Nome: {f1.Nome}");
+Console.WriteLine($"CPF: {f1.Cpf}");
+Console.WriteLine($"Salário: {f1.Salario:C}");
+Console.WriteLine($"Bonificação: {f1.getBonificacao():C}\n");
+
+Diretor d1 = new Diretor();
+d1.Nome = "Paula";
+d1.Cpf = "012.345.678-90";
+d1.Salario = 5000;
+
+Console.WriteLine($"Nome: {d1.Nome}");
+Console.WriteLine($"CPF: {d1.Cpf}");
+Console.WriteLine($"Salário: {d1.Salario:C}");
+Console.WriteLine($"Bonificação: {d1.getBonificacao():C}\n");
+
+Console.ReadKey();
