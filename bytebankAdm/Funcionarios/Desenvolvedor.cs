a38abe6695198534d652dd0d8f46342ab1df6ecc
Davi Kennedy Alves Rodrigues <davi_kennedy@live.com>
2022-12-08 23:10:07 -0300

    Adicionando a classe Desenvolvedor

---
+++ bytebankAdm/Funcionarios/Desenvolvedor.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace bytebankAdm.Funcionarios
+{
+    public class Desenvolvedor : Funcionario
+    {
+        public override double getBonificacao()
+        {
+            return Salario * 0.1;
+        }
+
+        public override double aumentarSalario()
+        {
+            return Salario *= 1.15;
+        }
+
+        public Desenvolvedor(string Cpf) : base(Cpf, 3000)
+        {
+
+        }
+    }
+}
